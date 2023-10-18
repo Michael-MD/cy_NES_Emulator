7a3a46e3a46c488d8f1a25fd68177c0bc254f68e
Michael-MD <mdei0001@student.monash.edu>
2023-10-19 07:29:31 +1100

    properly defined header

--- Mappers/Mapper.pyx
+++ Mappers/Mapper.pyx
@@ -3,6 +3,6 @@ cdef extern from "stdint.h":
 	ctypedef unsigned short uint16_t
 
 cdef class Mapper:
-	def __init__(self, prog_banks, char_banks):
+	def __cinit__(self, uint8_t prog_banks, uint8_t char_banks):
 		self.prog_banks = prog_banks
 		self.char_banks = char_banks
