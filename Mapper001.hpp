a8733da40cfb7c638ddf8e917a6bc0bcd9c7bea9
Michael-MD <mdei0001@student.monash.edu>
2023-11-07 22:55:08 +1100

    fixed syntax mistake

--- Mapper001.hpp
+++ Mapper001.hpp
@@ -7,7 +7,7 @@
 class Mapper001 : public Mapper
 {
 public:
-	Mapper001::Mapper001(uint8_t prog_banks, uint8_t char_banks);
+	Mapper001(uint8_t prog_banks, uint8_t char_banks);
 
 	uint8_t ppu_map_read(uint16_t addr, uint32_t *mapped_addr) override;
 	uint8_t ppu_map_write(uint16_t addr, uint32_t *mapped_addr) override;
